404a86d76521c003908adc3f3753df479694acb4
benedictgodhana <113383009+benedictgodhana@users.noreply.github.com>
2022-11-23 15:29:09 +0300

    Add files via upload

---
+++ number.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    /*Declare the variables and Initialize each of the variable*/
+    for (int a=20; a < 30; a=a+1)
+    {
+        cout<<"Value of a: "<<a<<endl;
+    }
+}
